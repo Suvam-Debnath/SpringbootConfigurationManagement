55a2ff1aefee808c824107089b29c29a36c939b6
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-11 12:36:41 +0530

    spring cloud config server initial commit

---
+++ ConfigServer/src/main/java/com/suvam/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.suvam.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
